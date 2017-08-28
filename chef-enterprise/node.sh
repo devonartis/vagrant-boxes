23c656af7063e3448b6b022c75455044af495da2
Devon Artis <devon@marketmemobi.com>
2017-08-28 07:29:10 -0400

    Updated directory

---
+++ chef-enterprise/node.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# configure hosts file for our internal network defined by Vagrantfile
+echo "Adding vagrant ip to host file"
+cat >> /etc/hosts <<EOL
+
+# vagrant environment nodes
+10.0.5.20  chefserver.dev
+10.0.5.30  chefworkstation.dev
+10.0.5.40  chefnode1.dev
+EOL
---
+++ chef-enterprise/wrkstation.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+# Setup workstation for Chef this is normally done on your own computer
+# Download and install chef development kit
+yum install -y wget
+wget https://packages.chef.io/files/stable/chefdk/2.1.11/el/7/chefdk-2.1.11-1.el7.x86_64.rpm
+rpm -Uvh  https://packages.chef.io/files/stable/chefdk/2.1.11/el/7/chefdk-2.1.11-1.el7.x86_64.rpm
+
+# configure hosts file for our internal network defined by Vagrantfile
+echo "Adding vagrant ip to host file"
+cat >> /etc/hosts <<EOL
+
+# vagrant environment nodes
+10.0.5.20  chefserver.dev
+10.0.5.30  chefworkstation.dev
+10.0.5.40  chefnode1.dev
+EOL
+
